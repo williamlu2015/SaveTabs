569a3f3c2387d4d15fa5ebe5ac6dbb1e0d0ed26d
William Lu <williamlu2015@alumni.ubc.ca>
2018-07-23 22:45:31 -0700

    Final commit

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
+/**
+ * Gets the URLs of all open tabs and saves them, with line breaks between URLs,
+ * to a file (in the default Downloads directory) with the current date and time
+ * as its name. Incognito tabs are included if the "Save incognito tabs"
+ * checkbox is checked.
+ */
 function saveUrls() {
     let incognitoInput = document.getElementById("incognito--input");
 
@@ -10,10 +16,15 @@ function saveUrls() {
         });
 
         let dateString = moment().format("YYYY-MM-DD_HH'mm'ss");
-        writeText(`SaveTabs/urls/${dateString}.txt`, urls);
+        writeText(`${dateString}.txt`, urls);
     });
 }
 
+/**
+ * Opens the URLs in the "URLs to restore" text area, opening a new tab for each
+ * URL. If a URL in the text area does not contain "://", then it is
+ * automatically prefixed with "http://".
+ */
 function restoreUrls() {
     let urlsTextarea = document.getElementById("urls--textarea");
     let urlsString = urlsTextarea.value;
@@ -30,6 +41,12 @@ function restoreUrls() {
     });
 }
 
+/**
+ * Adds "http://" to the front of the given URL, if the given URL does not
+ * contain "://".
+ * @param url   the url to pad with "http://"
+ * @returns {!string}   the url, with "http://" padding (if necessary)
+ */
 function httpPad(url) {
     if (!url.includes("://")) {
         return "http://" + url;
@@ -38,6 +55,12 @@ function httpPad(url) {
     }
 }
 
+/**
+ * Initializes the GUI.
+ * - Adds on-click event listeners to the "Save" and "Restore" buttons
+ * - Adds an on-input event listener to the "URLs to restore" textarea, so that
+ *   the "Restore" button is disabled whenever the textarea is empty
+ */
 (function() {
     let saveButton = document.getElementById("save--button");
     saveButton.addEventListener("click", saveUrls);
--- util/io.js
+++ util/io.js
@@ -1,3 +1,11 @@
+/**
+ * Writes the given data, using UTF-8 encoding, to the file with the given name
+ * in the default Downloads directory. Opens a "Save As" dialog to allow the end
+ * user to change the directory/file the data is saved to.
+ * @param filename   the name of the file in the default Downloads directory to
+ * save data to
+ * @param data   the data to save
+ */
 function writeText(filename, data) {
     let url = "data:text/plain;charset=utf-8," + encodeURIComponent(data);
     chrome.downloads.download({
